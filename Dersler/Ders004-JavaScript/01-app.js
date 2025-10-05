b23530cf8d0cc76689aac8781cdbb9e49ae27992
Sercan Kurban <sercan123@gmail.com>
2025-10-05 12:27:05 +0300

    JS Giriş

--- Dersler/Ders004-JavaScript/01-app.js
+++ Dersler/Ders004-JavaScript/01-app.js
@@ -11,7 +11,6 @@ console.log("Merhaba AcademyClub");
 console.log("Merhaba AcademyClub");
 */
 
-
 //********************Değişkenler********************
 // a = 10;
 // console.log(a);
@@ -26,7 +25,6 @@ console.log("Merhaba AcademyClub");
 // const c = 30;
 // c = 35; // Hata verir, değiştiremeyiz.
 // console.log(c);
-
 
 //********************Veri Tipleri********************
 //String
@@ -134,3 +132,143 @@ console.log("Merhaba AcademyClub");
 // a[0] = 60;
 // a.push(60);
 // console.log("2- ", a , b);
+
+//Scope - Kapsam
+// var a = 10; //Global Scope
+// function deneme(){
+//     var b = 20; //Function Scope
+//     c = 30; //Global Scope (Hata vermez ama kullanılması önerilmez.)
+//     console.log(a);
+//     console.log(b);
+// }
+// deneme();
+// console.log(a);
+// console.log(b); //Fonksiyon içerisinde olduğu için global alanda çalıştığında hata verecektir.
+// console.log(c);
+
+//Örnek Scope
+// if(true){
+//     var a = 10; //Function Scope
+//     let b = 20; //Block Scope
+//     const c = 30; //Block Scope
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//********************Tip Dönüşümleri********************
+// var a = 10;
+// console.log(a, typeof a);
+// var b = "20";
+// console.log(b, typeof b);
+// b = "Sercan10";
+// var sonuc = String(a);
+// var sonuc = Number(b);
+// console.log(sonuc, typeof(sonuc));
+
+//Örnek
+// var a = 5 + "2";
+// console.log(a, typeof a);
+// var a = 5 - "2";
+// console.log(a, typeof a);
+
+//********************Operatörler********************
+//Atama Operatörleri (=)
+// var a = 10;
+
+//Aritmetik Operatörler (+, -, *, /, %, **)
+// console.log(5 + 2); //Toplama
+// console.log(5 - 2); //Çıkarma
+// console.log(5 * 2); //Çarpma
+// console.log(5 / 2); //Bölme
+// console.log(5 % 2); //Mod Alma
+// console.log(5 ** 2); //Üslü
+
+//Karşılaştırma Operatörleri (==, ===, !=, !==, >, <, >=, <=)
+// console.log(2 == "2") //Değer Karşılaştırması
+// console.log(2 === "2") //Hem Değer Hem De Tip Karşılaştırması
+// console.log(2 != "2") //Değer Karşılaştırması - Eşit Değil
+// console.log(2 !== "2") //Hem Değer Hem De Tip Karşılaştırması Eşit Değil
+// console.log(2 > "2") //Büyük
+// console.log(2 < "2") //Küçük
+// console.log(2 >= "2") //Büyük Eşit
+// console.log(2 <= "2") //Küçük Eşit
+
+//Math Fonksiyonu
+// let sonuc;
+// sonuc = Math.PI;
+// sonuc = Math.round(3.6);
+// sonuc = Math.ceil(3.2);
+// sonuc = Math.floor(3.8);
+// sonuc = Math.sqrt(16);
+// sonuc = Math.floor(Math.random() * 100 + 1) // 0-100 arası rastgele sayı üretir.
+// sonuc = Math.random();
+// console.log(sonuc);
+
+//String Metotları
+// let value;
+// const firstName = "Sercan";
+// const lastName = "Kurban";
+// const city = "İstanbul";
+// const age = 29;
+
+// value = firstName + " " + lastName;
+// value = firstName;
+// value += " " + lastName;
+// value = firstName.length; // ismin kaç harf olduğunu gösterir
+// value = firstName.toLocaleLowerCase(); // tüm harfleri küçük harfe çevirir
+// value = firstName.toLocaleUpperCase(); // tüm harfleri büyük harfe çevirir
+// value = firstName[0]; // ismin ilk indeksi hariç harf sayısını gösterir
+// value = firstName[firstName.length - 1]; // ismin son harfini gösterir
+// value = firstName.indexOf("a"); // a harfini içerip içermediğini kontrol etme
+// value = firstName.includes("a"); // a harfini içerip içermediğini kontrol etme (true - false)
+// value = firstName.concat(" ", lastName,  " ", city); // birleştirme işlemi yapar
+// value = firstName.slice(0, 3); // 0'ıncı karakterden itibaren 3 karaktere kadar al (3 dahil değil)
+// console.log(value);
+
+// value =
+//   "İsim: " +
+//   firstName +
+//   "\nSoyisim: " +
+//   lastName +
+//   "\nŞehir: " +
+//   city +
+//   "\nYaş: " +
+//   age;
+// console.log(value);
+
+//Template Literals
+// `` = İşaretin Kısayolu: (ALT GR + Enter'in üzerindeki virgül olana 2 defa basın)
+// value = `İsim: ${firstName}
+// Soyisim: ${lastName}
+// Şehir: ${city}
+// Yaş: ${age}
+// `;
+// console.log(value);
+
+//********************Array Metotları********************
+// const langs = ["Python", "Java", "C++", "JavaScript"];
+// const numbers = [3, 5, 2, 10, 15, 7];
+// let value;
+// value = langs.length; //Eleman sayısı
+// value = langs[0]; //İlk eleman
+// value = langs[langs.length - 1]; //Son eleman
+// value = langs.indexOf("Java"); //Elemanın indeksi
+// value = langs.includes("Python"); //Eleman var mı?
+// value = langs.push("C#"); //Sona eleman ekler
+// value = langs.unshift("Ruby"); //Başa eleman ekler
+// value = langs.pop(); //Sondaki elemanı siler
+// value = langs.shift(); //Baştaki elemanı siler
+// value = langs.reverse(); //Diziyi ters çevirir
+// value = langs.sort(); //Diziyi alfabetik sıralar
+// value = numbers.sort(); //Diziyi alfabetik sıralar
+// value = numbers.sort(function(a, b){
+//     return a - b; //Küçükten büyüğe sıralar
+// });
+// value = numbers.sort(function(a, b){
+//     return b - a; //Büyükten küçüğe sıralar
+// });
+
+// console.log(numbers);
+// console.log(langs);
+// console.log(value);
