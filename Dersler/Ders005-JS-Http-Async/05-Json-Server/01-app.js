27ba07374490bd4781b3086da49c713f09e958d5
Sercan Kurban <sercan123@gmail.com>
2025-10-11 15:50:35 +0300

    Json-Server

---
+++ Dersler/Ders005-JS-Http-Async/05-Json-Server/01-app.js
@@ -0,0 +1,10 @@
+function getData(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((item) => {
+        console.log(item.title, item.author);
+      });
+    });
+}
+getData("http://localhost:3000/posts");
